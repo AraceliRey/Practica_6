c859a0921fa16da04955aabc34887c02ce972ba5
AraceliRey <ara_choko19@hotmail.es>
2014-05-22 00:12:07 -0500

    el mysql

---
+++ MySQL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace Practica6
+{
+	public class MySQL
+	{
+		protected MySqlConnection myConnection;
+		public MySQL ()
+		{
+		}
+
+		protected void abrirConexion(){
+			string connectionString =
+          		"Server=localhost;" +
+	          	"Database=Profesor;" +
+	          	"User ID=Araceli;" +
+	          	"Password=12345;" +
+	          	"Pooling=false;";
+	       this.myConnection = new MySqlConnection(connectionString);
+	       this.myConnection.Open();
+		}
+
+		protected void cerrarConexion(){
+			this.myConnection.Close(); 
+			this.myConnection = null;
+		}
+	}
+}
